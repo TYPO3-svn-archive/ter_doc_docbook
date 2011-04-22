d922f358960f1dc36d0c693d74f46141136125c4
speedprogs_de <speedprogs_de@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-22 17:31:40 +0000

    added dependency to ter_doc
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc_docbook/trunk@46934 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,11 @@ $EM_CONF[$_EXTKEY] = Array (
 	'download_password' => '',
 	'version' => '0.0.0',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:2:{s:9:"ChangeLog";s:4:"bc0a";s:12:"ext_icon.gif";s:4:"1bdc";}',
+	'constraints' => array(
+		'depends' => array(
+			'ter_doc' => '2.0.3-0.0.0',
+		),
+	),
 );
 
 ?>
